3714e6774dabc6e9467616999d60451113165548
Tamir <1tamir198@gmail.com>
2019-10-13 15:55:42 +0300

    Image not saving fix
    Fix bug that prevents saving the image

--- app/src/main/java/com/kotlin/pixlepaint/SaveImage.kt
+++ app/src/main/java/com/kotlin/pixlepaint/SaveImage.kt
@@ -1,19 +1,25 @@
 package com.kotlin.pixlepaint
+import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
 import android.provider.MediaStore
+import android.widget.Toast
 
 
 class SaveImage(
     private val bitmap: Bitmap, private val title: String,
-    private val description: String, private val context: Context) {
+    private val description: String, private val context: Context, private val activity:Activity) {
 
     fun saveImage() { //saves image to gallery
         println("Save image called")
+
+        Toast.makeText(context,"Saving Image",Toast.LENGTH_LONG).show()
+
+
         MediaStore.Images.Media.insertImage(
             context.contentResolver,
             bitmap, title,
-            description
-        )
+            description)
+
     }
 }
